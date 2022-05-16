fcb5ac14718e72fbd9c4fe40529de5b19556d47e
jackiehoon <wognsdl4545@naver.com>
2022-05-16 17:43:42 +0900

    Update Carousel.jsx

--- react-app/src/components/Bootstrap/Carousel.jsx
+++ react-app/src/components/Bootstrap/Carousel.jsx
@@ -1,29 +1,55 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Carousel = ({ data }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const handleClick = (i) => {
+    let nextIndex = activeIndex + i;
+    const lastIndex = data.length - 1;
+    if (nextIndex < 0) {
+      nextIndex = lastIndex;
+    } else if (nextIndex > lastIndex) {
+      nextIndex = 0;
+    }
+    setActiveIndex(nextIndex);
+  };
   return (
     <Container>
       <List>
-        {data.map(({ id, image }) => (
-          <Image key={id} src={image} />
+        {data.map(({ id, image }, index) => (
+          <Image key={id} src={image} isShow={index === activeIndex} />
         ))}
       </List>
-      <BtnPrev>이전</BtnPrev>
-      <BtnNext>다음</BtnNext>
+      <BtnPrev onClick={() => handleClick(-1)}>이전</BtnPrev>
+      <BtnNext onClick={() => handleClick(1)}>다음</BtnNext>
     </Container>
   );
 };
 
 const Container = styled.div`
   position: relative;
+  width: 800px;
+  height: 600px;
 `;
 const List = styled.div``;
 const Image = styled.img`
   position: absolute;
   width: 800px;
   height: 600px;
+  opacity: ${({ isShow }) => !isShow && "0"};
+  transition: opacity 1s;
 `;
-const BtnPrev = styled.button``;
-const BtnNext = styled.button``;
+
+const Btn = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+const BtnPrev = styled(Btn)`
+  left: 20px;
+`;
+const BtnNext = styled(Btn)`
+  right: 20px;
+`;
 
 export default Carousel;
