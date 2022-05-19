7cd3d13368012aa0a1155a68ea5760519578ee9d
jackiehoon <wognsdl4545@naver.com>
2022-05-19 17:47:03 +0900

    axios instance

--- naver/src/apis/index.js
+++ naver/src/apis/index.js
@@ -1,15 +1,28 @@
 import axios from "axios";
 
+const instance = axios.create({
+  headers: {
+    "X-Naver-Client-Id": "cXImhXldb32v4Yu5Hs9T",
+    "X-Naver-Client-Secret": "kcNwJta1kV",
+  },
+});
+
 export const getMovies = async (params) => {
   const config = {
     method: "GET",
     url: "/v1/search/movie.json",
-    headers: {
-      "X-Naver-Client-Id": "cXImhXldb32v4Yu5Hs9T",
-      "X-Naver-Client-Secret": "kcNwJta1kV",
-    },
     params,
   };
-  const result = await axios(config);
+  const result = await instance(config);
+  return result.data;
+};
+
+export const getBooks = async (params) => {
+  const config = {
+    method: "GET",
+    url: "/v1/search/book.json",
+    params,
+  };
+  const result = await instance(config);
   return result.data;
 };
